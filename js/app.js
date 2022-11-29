04b39cbf414ce8d6596da2df47a1f4b1c2b85975
Marwan051 <115355087+Marwan051@users.noreply.github.com>
2022-11-29 19:44:38 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,22 +1,54 @@
 //Defining global variables
- const n= document.querySelectorAll("section");
-const navbar = document.getElementById('navbar__list');
+const n = document.querySelectorAll("section");
+const navbar = document.getElementById(`navbar__list`);
 const lists = Array.from(n);
-/* build the nav
-I can't use preventDeault() since there's no events with the method I have used to scroll to sections.
-*/
-function buildnav(){
-for(list of lists){
-     item = document.createElement("li");
-    item.innerHTML=`<li><a href="#${list.id}" class="menu__link">${list.id}</a></li>`;/*add the list item in the ul and addind an anchor element to scroll to the section*/
-    navbar.appendChild(item);}}
+// function to build the nav
+function buildnav() {
+    for (const list of lists) {
+        item = document.createElement("li");
+        item.innerHTML = `<li class="menu__link" id="${list.id}">${list.id}</li>`;
+        navbar.appendChild(item);
+    }
+
+}
+//function to add scrolling functionallity
+function clickNscroll() {
+    const navli = document.getElementsByClassName("menu__link");
+    for (let i = 0; i <= navli.length - 1; i++) {
+        navli[i].addEventListener('click', (event) => {  
+            n[i].scrollIntoView({
+                behavior: "smooth",
+                block: "end",
+                inline: "nearest"
+            });//adding event listener to each link
+            event.preventDefault();
+            for (let i = 0; i <= navli.length - 1; i++) {
+                navli[i].classList.remove("active-link");//removing active link state
+            };
+            const s = event.target;
+            s.classList.add("active-link");//aading active link state
+        })
+    }
+}
+
+
 // Add class 'active' to section when near top of viewport
-document.addEventListener('scroll',()=>{
-const place = n.forEach((elem)=>{
-    const docheight= window.innerHeight;// getting height of viewport
-    const upheight= elem.getBoundingClientRect().bottom// getting height of bottom of each section
-if(docheight>=upheight&&docheight-upheight<=upheight){
-    elem.classList.add("your-active-class");
-}else {
-    elem.classList.remove("your-active-class");}})})
-buildnav();// Build menu 
+document.addEventListener('scroll', () => {
+
+
+    const place = n.forEach((elem) => {
+        const docheight = window.innerHeight; // getting height of viewport
+        const upheight = elem.getBoundingClientRect().bottom // getting height of bottom of each section
+
+        if (docheight >= upheight - 170 && docheight - upheight <= upheight + 170) {
+            elem.classList.add("your-active-class");
+
+
+        } else {
+            elem.classList.remove("your-active-class");
+        };
+    })
+})
+
+buildnav(); // Build menu 
+clickNscroll(); //add scrolling functionallity
